339613eb6abe201365ffa3775c27395a78bf7056
Ryo.N <ryo.nakano.eng@gmail.com>
2023-10-24 00:19:46 +0900

    4th commit

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title("Streamlit")
 
-df = pd.read_csv("/Users/owner/Desktop/Tech0/STEP2/Web_Application/20231009 ver1/02_前提知識/2a楽天api/hotel.csv")
+df = pd.read_csv("webapptest/hotel.csv")
 st.write(df)
